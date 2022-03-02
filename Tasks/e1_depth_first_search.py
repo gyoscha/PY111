28d7ef4c4473e2af60be445e3e89a31d5a7cec3d
gyoscha <sokolovgeorgy@gmail.com>
2022-03-02 19:50:49 +0300

    e1_depth_first_search.py DONE

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -10,5 +10,21 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    # вершина стэка справо
+    visited_nodes = {node: False for node in g.nodes}  # словарь из непосещенных узлов
+
+    wait_nodes = [start_node]  # стэк из узлов, которые ожидают обхода
+    visited_nodes[start_node] = True
+
+    path_nodes = []  # сюда должны помещаться узлы из стэка / "Сгоревшие узлы"
+
+    while wait_nodes:  # пока стэк существуют
+        current_node = wait_nodes.pop(-1)  # достаем горящий узел
+        path_nodes.append(current_node)
+
+        for neighbour in g[current_node]:
+            if not visited_nodes[neighbour]:
+                wait_nodes.append(neighbour)  # подожгли соседа
+                visited_nodes[neighbour] = True
+
+    return path_nodes
