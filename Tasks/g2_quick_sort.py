4d75a45c19e948a68f9358990bb25e0f6b8fac50
geosok <sokolovgeorgy@gmail.com>
2022-03-11 20:53:40 +0300

    not inplace quick sort Done

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -8,4 +8,14 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+    if not container:
+        return container
+
+    mid = container[len(container) // 2 - 1]   # можно выбрать как угодно!
+
+    left_part = [value for value in container if value < mid]
+    right_part = [value for value in container if value > mid]
+
+    middle_part = [value for value in container if value == mid]
+
+    return sort(left_part) + middle_part + sort(right_part)
