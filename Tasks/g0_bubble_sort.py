3376f03002e0f2f27e3e823b2411faa8640d2b03
geosok <sokolovgeorgy@gmail.com>
2022-03-11 18:54:22 +0300

    bubble_sort DONE

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -8,4 +8,24 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    # сортируем по возрастанию
+    offset = 1  # смещение относительно конца
+
+    for _ in range(len(container)):   # O(N)
+        is_change = False  # не было замены
+
+        for i in range(len(container) - offset):   # O(N)
+            if container[i] > container[i + 1]:
+                container[i], container[i + 1] = container[i + 1], container[i]
+                is_change = True
+
+        if not is_change:   # Если не было изменений, значит список отсортирован
+            break
+        offset += 1
+
     return container
+
+
+if __name__ == '__main__':
+    list_ = [-10, 200, 400, 0, -900, -60, 2]
+    print(sort(list_))
